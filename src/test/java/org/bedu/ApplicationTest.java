6c9bac167d994c3a0ccc53c90b3865b7a9af1a8b
Erendira-Garcia <iraisegv@gmail.com>
2024-01-24 20:29:17 -0600

    Archivos de testing sesion 7

--- src/test/java/org/bedu/ApplicationTest.java
+++ src/test/java/org/bedu/ApplicationTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTest {
 
+	// Smoke Test :  que el archivo se carga i.e. que arranca , que se inicializa
 	@Test
 	void contextLoads() {
 	}
